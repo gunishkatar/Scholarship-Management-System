4a8ea9b63fb091d717f2c1fe54ff6d0f3eb97f1e
Rahul Kodumuru <ksairahul15@gmail.com>
2021-07-07 19:08:36 +0530

    SM5308-59 : Project Arch Config

---
+++ src/main/java/com/dal/group7/shared/service/UserService.java
@@ -0,0 +1,19 @@
+package com.dal.group7.shared.service;
+
+
+/**
+ * <h1>UserService Interface</h1>
+ * The UserService Interface contains abstract methods that a user needs to implement
+ *
+ * @author : Sai Rahul Kodumuru
+ * @version : 1.0
+ * @since : 2021-July-05
+ */
+public interface UserService {
+
+    void signup();
+
+    void login();
+
+    void logout();
+}
