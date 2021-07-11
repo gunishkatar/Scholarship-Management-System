57266398972f828af15c7c39b4def9436c908c9b
Rishita Kotiyal <rs677988@dal.ca>
2021-07-11 23:43:48 +0530

    Refactored Institute class and added InstituteSignUp json file

--- src/main/java/com/dal/group7/persistent/model/Institute.java
+++ src/main/java/com/dal/group7/persistent/model/Institute.java
@@ -10,13 +10,31 @@ import static com.dal.group7.constants.FieldConstants.NAME;
 public class Institute {
     private Integer id;
     private String name;
+    private String emailId;
+    private Integer registrationCode;
+    private Integer phoneNumber;
+    private String address;
+    private String state;
+    private String city;
+    private String country;
+    private Integer pinCode;
 
     public Institute() {
     }
 
-    public Institute(Integer id, String name) {
+    public Institute(Integer id, String name, String emailId, Integer registrationCode,
+                     Integer phoneNumber, String address,String state, String city,String country,
+                     Integer pinCode) {
         this.id = id;
         this.name = name;
+        this.emailId = emailId;
+        this.registrationCode = registrationCode;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.state = state;
+        this.city = city;
+        this.country = country;
+        this.pinCode = pinCode;
     }
 
     public Institute from(ResultSet resultSet) throws SQLException {
@@ -31,5 +49,37 @@ public class Institute {
 
     public String getName() {
         return name;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public Integer getRegistrationCode() {
+        return registrationCode;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public Integer getPinCode() {
+        return pinCode;
     }
 }
