51804bfec6ffce867fbaa44e08aefef1cd196558
Sai Rahul Kodumuru <sr584403@dal.ca>
2021-07-15 16:55:25 +0530

    No JIRA: Removed the placeholder code

--- src/main/java/com/dal/group7/Application.java
+++ src/main/java/com/dal/group7/Application.java
@@ -1,17 +1,11 @@
 package com.dal.group7;
 
 import com.dal.group7.view.implementations.HomeCommand;
-import com.dal.group7.view.implementations.UserView;
 
 public class Application {
-    void init() {
-        UserView uv = new UserView();
-        uv.options();
-    }
 
     public static void main(String[] args) {
         final HomeCommand homeCommand = new HomeCommand();
         homeCommand.execute();
-//        new Application().init();
     }
 }
