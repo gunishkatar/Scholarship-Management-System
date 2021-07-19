8873f164f19397d13883d8f01b2c6cd62577d348
Gunish Katar <gn894984@dal.ca>
2021-07-19 11:25:41 -0300

    SM5308-74 : Implemented printView(), handle(), and setNextCommand() function in InstituteLoginCommand class.

--- src/main/java/com/dal/group7/view/implementations/InstituteLoginCommand.java
+++ src/main/java/com/dal/group7/view/implementations/InstituteLoginCommand.java
@@ -1,7 +1,47 @@
 package com.dal.group7.view.implementations;
 
-public class InstituteLoginCommand {
+import com.dal.group7.service.implementation.InstituteLoginService;
+import com.dal.group7.view.interfaces.Command;
+import static com.dal.group7.constants.ViewConstants.*;
+import static com.dal.group7.view.implementations.CommandFactory.*;
 
+public class InstituteLoginCommand extends Command {
 
+    private String userName;
+
+    private String password;
+
+    private final InstituteLoginService instituteLoginService;
+
+    private boolean success;
+
+    public InstituteLoginCommand(InstituteLoginService loginService) {
+
+        this.instituteLoginService = loginService;
+    }
+
+    @Override
+    public void printView() {
+        System.out.print(PROMPT_PREFIX + ENTER_USERNAME);
+        this.userName = scanner.nextLine();
+        System.out.print(PROMPT_PREFIX + PASSWORD);
+        this.password = scanner.nextLine();
+    }
+
+    @Override
+    public void handle() {
+        try {
+            instituteLoginService.userLogin(userName, password);
+            this.success = true;
+        } catch (Exception exception) {
+            System.out.println(PROGRAM_MESSAGE_PREFIX + exception.getMessage() + PROGRAM_MESSAGE_POSTFIX);
+            this.success = false;
+        }
+    }
+
+    @Override
+    public void setNextCommand() {
+        nextCommand = success ? INSTITUTE_HOME.getCommand() : ERROR.getCommand();
+    }
 
 }
