0fe93fc64f2420895e121456c703ce9716a918d3
Sai Rahul Kodumuru <sr584403@dal.ca>
2021-07-20 00:56:47 +0530

    SM5308-77 : created Student apply view

--- src/main/java/com/dal/group7/constants/ViewConstants.java
+++ src/main/java/com/dal/group7/constants/ViewConstants.java
@@ -25,28 +25,48 @@ public class ViewConstants {
   public static final String MINISTRY = "3. Ministry";
   public static final String MINISTRY_PORTAL = "WELCOME TO MINISTRY PORTAL";
   public static final String PASSWORD = "Enter Password: ";
-  public static final String PLEASE_SELECT_YOUR_OPTION = "Please select your option: ";
+  public static final String PLEASE_SELECT_YOUR_OPTION =
+          "Please select your option: ";
   public static final String PROGRAM_MESSAGE_POSTFIX = " >>>>>";
   public static final String PROGRAM_MESSAGE_PREFIX = "<<<<< ";
   public static final String PROMPT_PREFIX = ">> ";
-  public static final String SCHOLARSHIP_MANAGEMENT_SYSTEM = "WELCOME TO SCHOLARSHIP MANAGEMENT SYSTEM";
-  public static final String SCHOLARSHIP_SCHEMES = "1. List Scholarship Schemes";
+  public static final String SCHOLARSHIP_MANAGEMENT_SYSTEM =
+          "WELCOME TO SCHOLARSHIP MANAGEMENT SYSTEM";
+  public static final String SCHOLARSHIP_SCHEMES =
+          "1. List Scholarship Schemes";
   public static final String SIGN_UP_AS = "Sign Up As : ";
-  public static final String SIGNED_UP_AS_STUDENT = "Successfully signed up as student";
-  public static final String SIGNED_UP_AS_INSTITUTE = "Successfully signed up as institute";
+  public static final String SIGNED_UP_AS_STUDENT =
+          "Successfully signed up as student";
+  public static final String SIGNED_UP_AS_INSTITUTE =
+          "Successfully signed up as institute";
   public static final String SIGN_UP_OPT = "1. Sign up";
   public static final String STUDENT = "1. Student";
-  public static final String STUDENT_MENU = "As a student, You can now access the following menu";
-  public static final String INSTITUTE_MENU = "As an institute, You can now access the following menu";
-  public static final String APPROVE_THE_APPLICATION = "3. Approves the student application";
-  public static final String REJECT_THE_APPLICATION = "4. Rejects the student application";
-  public static final String SUCCESSFULLY_CREATED_SCHOLARSHIP = "Successfully Created the Scholarship. " +
-          "Students can avail it now.";
-  public static final String VIEW_APPROVED_STUDENT_APPLICATIONS = "1. View Approved Student Applications";
-  public static final String STUDENT_BANNED_MSG= "You have been banned from applying for Further Scholarships";
-  public static final String INVALID_CREDENTIALS = "You have entered invalid Credentials";
-  public static final String STUDENT_SOFT_BLOCK_MSG = "You have been Blocked Temporarily";
+  public static final String STUDENT_MENU =
+          "As a student, You can now access the following menu";
+  public static final String INSTITUTE_MENU =
+          "As an institute, You can now access the following menu";
+  public static final String APPROVE_THE_APPLICATION =
+          "3. Approves the student application";
+  public static final String REJECT_THE_APPLICATION =
+          "4. Rejects the student application";
+  public static final String SUCCESSFULLY_CREATED_SCHOLARSHIP =
+          "Successfully Created the Scholarship. " +
+                  "Students can avail it now.";
+  public static final String VIEW_APPROVED_STUDENT_APPLICATIONS =
+          "1. View Approved Student Applications";
+  public static final String STUDENT_BANNED_MSG =
+          "You have been banned from applying for Further Scholarships";
+  public static final String INVALID_CREDENTIALS =
+          "You have entered invalid Credentials";
+  public static final String STUDENT_SOFT_BLOCK_MSG =
+          "You have been Blocked Temporarily";
   public static final String STUDENT_HARD_BLOCK_MSG = "You have been Blocked";
+  public static final String ENTER_APPLY_SCHEME_FILE =
+          "Enter the full path of the scholarship application";
+  public static final String APPLYING_SCHOLARSHIP =
+          "Trying to apply for a scholarship... after checking eligibility";
+  public static final String APPLICATION_SUBMITTED =
+          "Your application has been submitted Successfully...";
 
 
   private ViewConstants() {
