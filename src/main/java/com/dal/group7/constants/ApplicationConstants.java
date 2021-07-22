6f3dbb0e8e56c70db9952eecbe3e5f29774f4ce7
Gunish Katar <gn894984@dal.ca>
2021-07-22 19:56:21 -0300

    SM5308-88 : Added required constants in ApplicationConstants

--- src/main/java/com/dal/group7/constants/ApplicationConstants.java
+++ src/main/java/com/dal/group7/constants/ApplicationConstants.java
@@ -7,6 +7,10 @@ public class ApplicationConstants {
     public static final String DB_PASSWORD = "db.password";
     public static final String JOIN_USER = "?user=";
     public static final String JOIN_PASSWORD = "&password=";
+    public static final int AWARD_CAP = 10;
+    public static final double POINT_CAP = 25;
+    public static final double POINT_FACTOR = 2.5;
+    public static final double RANGE_FACTOR = 1.34;
 
     private ApplicationConstants() {
     }
