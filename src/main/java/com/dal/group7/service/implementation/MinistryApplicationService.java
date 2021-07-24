a5104902539fc4c3c6ea3ba5469ef9a099e0ba04
Sai Rahul Kodumuru <sr584403@dal.ca>
2021-07-24 21:09:47 +0530

    SM5308-83 : Added services

--- src/main/java/com/dal/group7/service/implementation/MinistryApplicationService.java
+++ src/main/java/com/dal/group7/service/implementation/MinistryApplicationService.java
@@ -1,12 +1,10 @@
 package com.dal.group7.service.implementation;
 
 import com.dal.group7.persistent.interfaces.Dao;
-import com.dal.group7.persistent.model.Application;
-import com.dal.group7.persistent.model.Institute;
-import com.dal.group7.persistent.model.Scholarship;
-import com.dal.group7.persistent.model.StudentFinance;
+import com.dal.group7.persistent.model.*;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import static com.dal.group7.persistent.model.ApplicationStatus.FUND_ISSUED;
 import static com.dal.group7.persistent.model.ApplicationStatus.REJECTED;
@@ -18,15 +16,18 @@ public class MinistryApplicationService {
     private final Dao<Integer, Scholarship> scholarshipDao;
     private final Dao<String, StudentFinance> studentFinanceDao;
     private final Dao<Integer, Institute> instituteDao;
+    private final Dao<Integer, StudentFeedback> studentFeedbackDao;
 
     public MinistryApplicationService(Dao<String, Application> applicationDao,
                                       Dao<Integer, Scholarship> scholarshipDao,
                                       Dao<String, StudentFinance> studentFinanceDao,
-                                      Dao<Integer, Institute> instituteDao) {
+                                      Dao<Integer, Institute> instituteDao,
+                                      Dao<Integer, StudentFeedback> studentFeedbackDao) {
         this.applicationDao = applicationDao;
         this.scholarshipDao = scholarshipDao;
         this.studentFinanceDao = studentFinanceDao;
         this.instituteDao = instituteDao;
+        this.studentFeedbackDao = studentFeedbackDao;
     }
 
     public Application getApplicationWithAmount(String applicationId) throws SQLException {
@@ -71,7 +72,7 @@ public class MinistryApplicationService {
 
     public void issueTenPercentGrantToInstitute(Application application)
             throws SQLException {
-        
+
         double grantAmount = calculateTenPercentGrantAmount(application);
 
         instituteDao
@@ -87,5 +88,22 @@ public class MinistryApplicationService {
 
         return 0.1 * (issuedInsuranceAmount + issuedTravelAmount +
                 issuedTuitionAmount);
+    }
+
+    public void awardInstitutes() throws Exception {
+        ArrayList<Integer> institutes = studentFeedbackDao.fetchValues();
+
+        if (institutes.isEmpty()) {
+            throw new Exception("No Institutes Found");
+        } else {
+            Integer instituteIdOne = institutes.get(0);
+            Integer instituteIdTwo = institutes.get(1);
+            instituteDao
+                    .incrementValue(instituteIdOne, GRANT_AMOUNT,
+                            0.05);
+            instituteDao
+                    .incrementValue(instituteIdTwo, GRANT_AMOUNT,
+                            0.03);
+        }
     }
 }
